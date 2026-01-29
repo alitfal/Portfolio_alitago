d7090a2fbad52f6e0e072298ec25b1e7733fabea
alitfal <angel.litago@me.com>
2026-01-29 18:28:11 +0000

    Mejoras en pie de porfolio

--- App/src/components/layout/footer/Footer.jsx
+++ App/src/components/layout/footer/Footer.jsx
@@ -1,32 +1,73 @@
-import React from 'react';
-import '@/styles/footer.css';
-import { useTranslation } from '@/components/context/translation/Translation.jsx';
+import React from "react";
+import "@/styles/footer.css";
+import { useTranslation } from "@/components/context/translation/Translation.jsx";
 
 const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className='footer'>
-      <div className='footer__container container'>
-        <h1 className='footer__title'>Angel Litago</h1>
-        <ul className='footer__list'>
-          <li><a href='#about' className='footer__link'>{t('footer.about')}</a></li>
-          <li><a href='#services' className='footer__link'>{t('footer.services')}</a></li>
-          <li><a href='#projects' className='footer__link'>{t('footer.projects')}</a></li>
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Angel Litago</h1>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              {t("footer.about")}
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="footer__link">
+              {t("footer.services")}
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="footer__link">
+              {t("footer.projects")}
+            </a>
+          </li>
         </ul>
-        <div className='footer__social'>
-          <a href='https://www.linkedin.com/in/alitago' className='footer__social-link' target='_blank' rel='noreferrer'><i className='bx bxl-linkedin' /></a>
-          <a href='https://github.com/alitfal' className='footer__social-link' target='_blank' rel='noreferrer'><i className='bx bxl-github' /></a>
-          <a href='https://hub.docker.com/u/alitago' className='footer__social-link' target='_blank' rel='noreferrer'><i className='bx bxl-docker' /></a>
-          <a href='https://paypal.me/alitago' className='footer__social-link' target='_blank' rel='noreferrer'><i className='bx bxl-paypal' /></a>
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com/in/alitago"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-linkedin" />
+          </a>
+          <a
+            href="https://github.com/alitfal"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-github" />
+          </a>
+          {/* <a href='https://hub.docker.com/u/alitago' className='footer__social-link' target='_blank' rel='noreferrer'><i className='bx bxl-docker' /></a> */}
+          <a
+            href="https://paypal.me/alitfal"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-paypal" />
+          </a>
         </div>
-        <span className='footer__copy'>
+        <span className="footer__copy">
           Copyright &#169; 2025 Angel Luis
           <br />
           Litago Falces - MIT License.
           <br />
           <br />
-          Powered by <a href='https://vercel.com/' className='footer__link' target='_blank' rel='noreferrer'>&#x25B2;Vercel</a>
+          Powered by{" "}
+          <a
+            href="https://vercel.com/"
+            className="footer__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            &#x25B2;Vercel
+          </a>
         </span>
       </div>
     </footer>
