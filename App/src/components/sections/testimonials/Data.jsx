04fdb5bfb9db12079574efa36e527806bc047aaa
alitfal <angel.litago@me.com>
2026-03-02 18:02:50 +0000

    mejoras en apartado testimonials

--- App/src/components/sections/testimonials/Data.jsx
+++ App/src/components/sections/testimonials/Data.jsx
@@ -1,23 +1,33 @@
 const customersData = [
   {
     id: 1,
-    imageLight: '/images/customer/light.png',
-    imageDark: '/images/customer/dark.png'
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
   },
   {
     id: 2,
-    imageLight: '/images/customer/light.png',
-    imageDark: '/images/customer/dark.png'
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
   },
   {
     id: 3,
-    imageLight: '/images/customer/light.png',
-    imageDark: '/images/customer/dark.png'
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
   },
   {
     id: 4,
-    imageLight: '/images/customer/light.png',
-    imageDark: '/images/customer/dark.png'
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
+  },
+  {
+    id: 5,
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
+  },
+  {
+    id: 6,
+    imageLight: "/images/customer/light.png",
+    imageDark: "/images/customer/dark.png",
   },
 ];
 
